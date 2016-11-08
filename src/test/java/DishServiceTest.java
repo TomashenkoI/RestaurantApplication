f145161817c744db10c1527b4afb5d22350abfa2
snitelol <TomashenkoI@gmail.com>
2016-11-08 10:59:29 +0200

    add tests

--- src/test/java/DishServiceTest.java
+++ src/test/java/DishServiceTest.java
@@ -1,2 +1,75 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+import ua.goit.java.dao.model.Dish;
+import ua.goit.java.service.DishService;
+
+import java.util.List;
+
+@ContextConfiguration(locations = "classpath:application-context.test.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
 public class DishServiceTest {
+
+    private DishService dishService;
+
+    @Before
+    public void setUp(){
+        List<Dish> all = dishService.getDishes();
+        all.stream().forEach(e -> dishService.delete(e.getID()));
+
+        Dish dish = new Dish();
+        dish.setName("test dish");
+        dishService.save(dish);
+
+        Dish dish2 = new Dish();
+        dish2.setName("test dish 2");
+        dishService.save(dish2);
+    }
+
+    @Test
+    public void save() throws Exception {
+        Dish dish = new Dish();
+        dish.setName("test dish 3");
+        dishService.save(dish);
+
+        List<Dish> all = dishService.getDishes();
+        Assert.assertEquals(all.size(), 3);
+    }
+
+    @Test
+    public void delete() throws Exception {
+        dishService.delete(2);
+
+        List<Dish> all = dishService.getDishes();
+        Assert.assertEquals(all.size(), 1);
+    }
+
+    @Test
+    public void getByName() throws Exception {
+        List<Dish> all = dishService.getDishByName("test dish");
+        Assert.assertEquals(all.size(), 2);
+    }
+
+    @Test
+    public void getById() throws Exception {
+        List<Dish> all = dishService.getDishes();
+        Dish dish = all.get(0);
+        String expectedName = dish.getName();
+
+        dish = dishService.getDishByID(dish.getID());
+
+        Assert.assertEquals(dish.getName(), expectedName);
+    }
+
+    @Test
+    public void getAll() throws Exception {
+        List<Dish> all = dishService.getDishes();
+        Assert.assertEquals(all.size(), 2);
+    }
+
 }
--- src/test/java/EmployeeServiceTest.java
+++ src/test/java/EmployeeServiceTest.java
@@ -1,4 +1,79 @@
 package java;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+import ua.goit.java.dao.model.Employee;
+import ua.goit.java.service.EmployeeService;
+
+import java.util.List;
+
+@ContextConfiguration(locations = "classpath:application-context-test.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
 public class EmployeeServiceTest {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @Before
+    public void setUp() throws Exception {
+        List<Employee> all = employeeService.getEmployees();
+        all.stream().forEach(e -> employeeService.deleteEmployee(e));
+
+        Employee employee = new Employee();
+        employee.setFirstName("test employee");
+        employeeService.save(employee);
+
+        Employee employee2 = new Employee();
+        employee2.setFirstName("test employee 2");
+        employeeService.save(employee2);
+    }
+
+    @Test
+    public void save() throws Exception {
+        Employee employee = new Employee();
+        employee.setFirstName("test employee 3");
+        employeeService.save(employee);
+
+        List<Employee> all = employeeService.getEmployees();
+        Assert.assertEquals(all.size(), 3);
+    }
+
+    @Test
+    public void delete() throws Exception {
+        employeeService.delete(2);
+
+        List<Employee> all = employeeService.getEmployees();
+        Assert.assertEquals(all.size(), 1);
+    }
+
+    @Test
+    public void getByName() throws Exception {
+        List<Employee> all = employeeService.getEmployeesByName("test employee");
+        Assert.assertEquals(all.size(), 2);
+    }
+
+    @Test
+    public void getById() throws Exception {
+        List<Employee> all = employeeService.getEmployees();
+        Employee employee = all.get(0);
+        String expectedName = employee.getFirstName();
+
+        employee = employeeService.getEmployeeByID(employee.getID());
+
+        Assert.assertEquals(employee.getFirstName(), expectedName);
+    }
+
+    @Test
+    public void getAll() throws Exception {
+        List<Employee> all = employeeService.getEmployees();
+        Assert.assertEquals(all.size(), 2);
+    }
+
 }
